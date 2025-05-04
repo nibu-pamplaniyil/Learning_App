8053e53d0135e6edbf28fcd7ecd0b4125ba5751a
nibu-pamplaniyil <nibupamplaniyil@gmail.com>
2025-05-04 15:43:26 +0530

    Resume view from UI

--- learn-frontend/src/app/hero/hero.component.ts
+++ learn-frontend/src/app/hero/hero.component.ts
@@ -1,37 +1,49 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SafeUrlModule } from './safe-url.module';
 
 @Component({
   selector: 'app-hero',
   standalone:true,
-  imports:[RouterModule],
+  imports:[RouterModule,CommonModule,SafeUrlModule],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent implements OnInit{
+export class HeroComponent implements OnInit {
   greeting: string = 'HELLO!';
   name: string = 'I Am Nibu George';
-  experience:number=0;
+  experience: number = 0;
   descriptions: string = '';
-  designation:string='';
+  resume: string = '';
+  designation: string = '';
   viewWorkLink: string = '/portfolio'; 
   hireMeLink: string = '/contact';   
   profileImage: string = '';
+  resumeVisible = false;
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<any>('http://localhost:5263/api/profile').subscribe(x=>{
-
+    this.http.get<any>('http://localhost:5263/api/profile').subscribe(x => {
       const data = x?.data?.[0];
-      if(data)
-      {
-        this.experience=data.experience;
-        this.profileImage=data.imageURL;
-        this.designation=data.designation;
+      if (data) {
+        this.experience = data.experience;
+        this.profileImage = data.imageURL;
+        this.designation = data.designation;
+        this.resume = data.resume;
         this.descriptions = `I'm a ${this.designation} with ${this.experience} years of experience. My expertise is in web development and more...`;
       }
     });
+  }
+  
+
+  viewResume(): void {
+    if (this.resume) {
+      this.resumeVisible = !this.resumeVisible;
+    } else {
+      alert('Resume not available');
+    }
   }
 }
